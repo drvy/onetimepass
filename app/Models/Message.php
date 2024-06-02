66fbe13a47971178fb02e24ac99ef0fce87208e8
Dragomir Yordanov <dyordanov@drvy.net>
2024-06-02 17:18:14 +0200

    Create Message model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use DateTime;
 use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
+    const CREATED_AT = 'createdAt';
+    const UPDATED_AT = 'updatedAt';
 
+
+    public function getCreatedAtAttribute($date)
+    {
+        return (new DateTime($date))->format('Y-m-d H:i:s');
+    }
+
+
+    public function getUpdatedAtAttribute($date)
+    {
+        return (new DateTime($date))->format('Y-m-d H:i:s');
+    }
 }
