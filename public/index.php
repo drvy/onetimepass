30d23a8bbf1b5a4c0a1dbb38dc5617f07559488e
Dragomir Yordanov <dyordanov@drvy.net>
2025-06-02 12:04:47 +0200

    Upgrades

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 define('ABSPATH', realpath(__DIR__ . '/../'));
 
 require_once ABSPATH . '/vendor/autoload.php';
-require_once ABSPATH . '/app/App.php';
+require_once ABSPATH . '/app/app.php';
 
 if (php_sapi_name() !== 'cli') {
     $app->run();
